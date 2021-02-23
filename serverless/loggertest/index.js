4d02fd97d938cd5aa78b8d5c3da55dc9226316d1
Nathan Morris <NMorris@uwhealth.org>
2021-02-23 09:38:16 -0600

    try catch

--- serverless/loggertest/index.js
+++ serverless/loggertest/index.js
@@ -6,70 +6,79 @@ console.log = logInfo;
 console.error = logError;
 
 exports.handler = async (event) => {
-  logger.on("error", consoleError);
-  const s = encodeURI(event.queryStringParameters.s);
-  const rawURIString = event.queryStringParameters.uri || event.path;
-  // sanitize input uri
-  const uriString = encodeURI(rawURIString);
+  try {
+    logger.on("error", consoleError);
+    const s = encodeURI(event.queryStringParameters.s);
+    const rawURIString = event.queryStringParameters.uri || event.path;
+    // sanitize input uri
+    const uriString = encodeURI(rawURIString);
 
-  // Your code here
-  console.log(`${s}: Info log for ${uriString}`);
-  console.log({
-    s: s,
-    uri: uriString,
-    example: "this is a sample object log",
-    nest: {
-      bird: {
-        type: "robin",
-        state: "Wisconsin",
+    // Your code here
+    console.log(`${s}: Info log for ${uriString}`);
+    console.log({
+      s: s,
+      uri: uriString,
+      example: "this is a sample object log",
+      nest: {
+        bird: {
+          type: "robin",
+          state: "Wisconsin",
+        },
+        food: "worm",
       },
-      food: "worm",
-    },
-  });
-  console.error(`${s}: Error log for ${uriString}`);
+    });
+    console.error(`${s}: Error log for ${uriString}`);
 
-  // Ensure logs have been flushed to LogDNA before finishing
-  await once(logger, "cleared");
-  switch (s) {
-    case "301":
-      return {
-        statusCode: 301,
-        headers: {
-          Location: `https://www.google.com`,
-          "Cache-Control": "public, max-age=0, must-revalidate",
-          "x-xss-protection": "1; mode=block",
-          "x-frame-options": "SAMEORIGIN",
-        },
-      };
-    case "301-404":
-      return {
-        statusCode: 301,
-        headers: {
-          Location: `/notactuallyhere`,
-          "Cache-Control": "public, max-age=0, must-revalidate",
-          "x-xss-protection": "1; mode=block",
-          "x-frame-options": "SAMEORIGIN",
-        },
-      };
-    case "404":
-      return {
-        statusCode: 404,
-        body: `Not found wahh wahh wahhhhhhhh.`,
-        headers: {
-          "Cache-Control": "public, max-age=0, must-revalidate",
-          "x-xss-protection": "1; mode=block",
-          "x-frame-options": "SAMEORIGIN",
-        },
-      };
-    default:
-      return {
-        statusCode: 200,
-        body: `Does the happy 200 reponse Log correctly?`,
-        headers: {
-          "Cache-Control": "public, max-age=0, must-revalidate",
-          "x-xss-protection": "1; mode=block",
-          "x-frame-options": "SAMEORIGIN",
-        },
-      };
+    // Ensure logs have been flushed to LogDNA before finishing
+    await once(logger, "cleared");
+    switch (s) {
+      case "301":
+        return {
+          statusCode: 301,
+          headers: {
+            Location: `https://www.google.com`,
+            "Cache-Control": "public, max-age=0, must-revalidate",
+            "x-xss-protection": "1; mode=block",
+            "x-frame-options": "SAMEORIGIN",
+          },
+        };
+      case "301-404":
+        return {
+          statusCode: 301,
+          headers: {
+            Location: `/notactuallyhere`,
+            "Cache-Control": "public, max-age=0, must-revalidate",
+            "x-xss-protection": "1; mode=block",
+            "x-frame-options": "SAMEORIGIN",
+          },
+        };
+      case "404":
+        return {
+          statusCode: 404,
+          body: `Not found wahh wahh wahhhhhhhh.`,
+          headers: {
+            "Cache-Control": "public, max-age=0, must-revalidate",
+            "x-xss-protection": "1; mode=block",
+            "x-frame-options": "SAMEORIGIN",
+          },
+        };
+      default:
+        return {
+          statusCode: 200,
+          body: `Does the happy 200 reponse Log correctly?`,
+          headers: {
+            "Cache-Control": "public, max-age=0, must-revalidate",
+            "x-xss-protection": "1; mode=block",
+            "x-frame-options": "SAMEORIGIN",
+          },
+        };
+    }
+  } catch (err) {
+    console.error(`500 error: ${err.message}`);
+    return {
+      statusCode: 500,
+      body:
+        "There has been an error with this request. Please view logs to determine cause.",
+    };
   }
 };
